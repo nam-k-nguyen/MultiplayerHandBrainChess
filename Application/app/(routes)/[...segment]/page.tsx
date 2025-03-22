17aea251e801f8ca40e90e9fcd0e1ca78030bd64
Nam Khanh Nguyen <nguye2nk@mail.uc.edu>
2025-03-22 17:08:06 -0400

    Delete old app folder

--- Application/app/(routes)/[...segment]/page.tsx
+++
@@ -1,5 +0,0 @@
-export default function UnknownPage({ params }: { params: { segment: string[] } }) {
-    return (
-        <p>Unknown segment {params.segment.join('/')}</p>
-    )
-}
--- Application/app/_components/Footer.tsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-    return (
-        <footer className={`text-orangered bg-whitesmoke px-5 py-2.5`}>
-            Alright, you are reserved &copy;
-        </footer>
-    )
-}
